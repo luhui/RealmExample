a69f851ad83072b0e56c99196a9f1a5adf322472
陆晖 <luhui@cvte.cn>
2016-02-17 10:51:42 +0800

    add clean test for executeTransaction success callback

--- app/src/main/java/com/cvte/realmexample/MyApplication.java
+++ app/src/main/java/com/cvte/realmexample/MyApplication.java
@@ -1,6 +1,11 @@
 package com.cvte.realmexample;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.cvte.realmexample.model.Users;
+
+import java.util.UUID;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -13,5 +18,24 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Realm.setDefaultConfiguration(new RealmConfiguration.Builder(this).deleteRealmIfMigrationNeeded().build());
+
+        Realm realm = Realm.getDefaultInstance();
+        Log.e("lh", "before save " + realm.allObjects(Users.class).size());
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                Users users = new Users();
+                users.setId(UUID.randomUUID().toString());
+                users.setName("test");
+                realm.copyToRealm(users);
+            }
+        }, new Realm.Transaction.Callback() {
+            @Override
+            public void onSuccess() {
+                //realm already update
+                Log.e("lh", "after save " + realm.allObjects(Users.class).size());
+                realm.close();
+            }
+        });
     }
 }
